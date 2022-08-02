b45b02e4bff9bde06518f8017bc372a30fe203e3
Liberbell <liberbell@gmail.com>
2022-08-03 08:51:30 +0900

    lesson46

--- lesson4.py
+++ lesson4.py
@@ -8,8 +8,10 @@ print("say \"I don't know\"")
 print('hello. \n How are you.')
 print(r'c:\name\name')
 
-print("""
+print("##########")
+print("""\
 line1
 line2
-line3
+line3\
 """)
+print("##########")
