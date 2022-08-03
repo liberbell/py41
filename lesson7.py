d40ae7bfe7351ce34cd8ed64ef64f0b55056a782
Liberbell <liberbell@gmail.com>
2022-08-04 08:40:33 +0900

    lesson73

--- lesson7.py
+++ lesson7.py
@@ -6,3 +6,4 @@ print('a is {2} {1} {0}'.format(1, 2, 3))
 
 print('My name is {0} {1}.'.format('Eric', 'Clapton'))
 print('My name is {0} {1}. I am {1} {0}.'.format('Eric', 'Clapton'))
+print('My name is {name} {family}. I am {family} {name}.'.format(name='Eric', family='Clapton'))
