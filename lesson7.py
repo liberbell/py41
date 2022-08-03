92d7211c01e114638604ff4540d3d276eff8045f
Liberbell <liberbell@gmail.com>
2022-08-04 08:37:38 +0900

    lesson72

--- lesson7.py
+++ lesson7.py
@@ -2,3 +2,6 @@ print('a is {}'.format('a'))
 
 print('a is {}'.format('test'))
 print('a is {} {} {}'.format(1, 2, 3))
+print('a is {2} {1} {0}'.format(1, 2, 3))
+
+print('My name is {}.'.format('Bob'))
