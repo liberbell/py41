005e6d55d105e436b8bcf1f76f22b6d2c8082fb4
Liberbell <liberbell@gmail.com>
2022-08-05 08:35:53 +0900

    lesson86

--- lesson8.py
+++ lesson8.py
@@ -2,17 +2,22 @@ from operator import le
 
 
 l = [1, 20, 12, 4, 5, 8]
-print(l[0])
-print(type(l))
-print(len(l))
-print(list("abcdefg"))
+# print(l[0])
+# print(type(l))
+# print(len(l))
+# print(list("abcdefg"))
 
 n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(n[::2])
-print(n[::-1])
+# print(n[::2])
+# print(n[::-1])
 
 a = ["a", "b", "c"]
 n = [1, 2, 3]
 x = [a, n]
-print(type(x))
-print(x[1][2])
+# print(type(x))
+# print(x[1][2])
+
+s = ["a", "b", "c", "d", "e", "f", "g"]
+print(s[0])
+s[0] = "x"
+print(s)
