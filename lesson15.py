82431faf890f3cec8715e07e58aa423ecb2342d0
Liberbell <liberbell@gmail.com>
2022-08-08 08:41:12 +0900

    lesson153

--- lesson15.py
+++ lesson15.py
@@ -1,9 +1,22 @@
 x = {"a": 1}
 y = x
 y["a"] = 100
-print(x, y)
+# print(x, y)
 
 x = {"a": 1}
 y = x.copy()
 y["a"] = 100
-print(x, y)
+# print(x, y)
+
+fruits = {
+    "apple": 100,
+    "banana": 200,
+    "orange": 300,
+    }
+print(fruits["apple"])
+
+l = [
+    ["apple", 100],
+    ["banana", 200],
+    ["orange", 300],
+]
