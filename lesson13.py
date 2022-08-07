b0f2c96f1da6126ebed86cf986b747190852af3a
Liberbell <liberbell@gmail.com>
2022-08-07 14:17:17 +0900

    lesson133

--- lesson13.py
+++ lesson13.py
@@ -1,4 +1,8 @@
-choose_from_TWO = ("A", "B", "C")
+# choose_from_TWO = ("A", "B", "C")
+choose_from_TWO = ["A", "B", "C"]
+
+choose_from_TWO.append("A")
+choose_from_TWO.append("C")
 
 answer = []
 answer.append("A")
