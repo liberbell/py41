2371a751288c364ddd656e66c350e32afdf65e65
Liberbell <liberbell@gmail.com>
2022-08-08 08:50:05 +0900

    lesson164

--- lesson16.py
+++ lesson16.py
@@ -1,11 +1,14 @@
 a = {1, 2, 5, 6, 9, 2, 6, 1, 6, 5}
-print(a, type(a))
+# print(a, type(a))
 
 b = {4, 2, 7, 9, 7, 4, 2, 5, 0}
-print(b, type(b))
+# print(b, type(b))
 
-print(a - b)
-print(b - a)
-print(a & b)
-print(a | b)
-print(a ^ b)
+# print(a - b)
+# print(b - a)
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+
+a.add(10)
+print(a)
