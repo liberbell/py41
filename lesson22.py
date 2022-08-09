d9e8675d925d81c4dd792eccfcb59e13466ddfeb
Liberbell <liberbell@gmail.com>
2022-08-10 08:18:31 +0900

    lesson225

--- lesson22.py
+++ lesson22.py
@@ -3,11 +3,17 @@ count = 0
 #     print(count)
 #     count += 1
 
-while True:
-    if count >= 5:
-        break
-    if count == 2:
-        count += 1
-        continue
+# while True:
+#     if count >= 5:
+#         break
+#     if count == 2:
+#         count += 1
+#         continue
+#     print(count)
+#     count += 1
+
+while count < 5:
     print(count)
     count += 1
+else:
+    print("done.")
