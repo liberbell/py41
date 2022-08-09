a27c5ff63a0e9f5039846e0533dd9064f98b61fb
Liberbell <liberbell@gmail.com>
2022-08-09 14:04:03 +0900

    lesson187

--- lesson18.py
+++ lesson18.py
@@ -6,7 +6,7 @@ x = 1 + 1 + 1 + 1 + 1 + 1 + 1 \
     + 1 + 1 + 1 + 1 + 1 + 1 + 1
 # print(x)
 
-x = 0
+x = 10
 if x < 0:
     print("negative")
 elif x == 0:
@@ -15,3 +15,10 @@ elif x == 10:
     print("ten")
 else:
     print("positive")
+
+a = 5
+b = 10
+if a > 0:
+    print("a is positive")
+    if b > 0:
+        print("b is positive")
