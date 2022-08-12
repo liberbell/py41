47dfaf108be3ebfb14bc35bedae1550819a6159c
Liberbell <liberbell@gmail.com>
2022-08-12 17:57:36 +0900

    lesson251

--- lesson25.py
+++ lesson25.py
@@ -1,9 +1,10 @@
-d = {"x": 100, "y": 200}
+# d = {"x": 100, "y": 200}
 
-for k, v in d.items():
-    print(k, ";", v)
+# for k, v in d.items():
+#     print(k, ";", v)
 
 def say_something():
     print("hi")
 
 say_something()
+print(type(say_something))
