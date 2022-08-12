9d855356c0934d973e4fb7f077f38ddf15e16941
Liberbell <liberbell@gmail.com>
2022-08-12 18:10:50 +0900

    lesson264

--- lesson26.py
+++ lesson26.py
@@ -3,5 +3,10 @@ num: int = 10
 def add_num(a: int, b:int) -> int:
     return a + b
 
-r = add_num(10, 20)
-print(r)
+r = add_num("a", "b")
+# print(r)
+
+def menu(entree):
+    print(entree)
+
+menu("beef")
