808275d333d63e71c6ccb0fe6a280556fb4d542b
Liberbell <liberbell@gmail.com>
2022-08-14 13:55:17 +0900

    lesson306

--- lesson30.py
+++ lesson30.py
@@ -20,9 +20,17 @@
 # print(cal1(10))
 # print(cal2(10))
 
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        result = func(args, kwargs)
+        return result
+    return wrapper
+
 def add_num(a, b):
     return a + b
 
 print("start")
 r = add_num(10, 20)
+print("end")
+
 print(r)
