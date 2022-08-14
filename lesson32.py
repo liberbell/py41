c5a8743fa11d216fbc327b9ace621d6ea5746531
Liberbell <liberbell@gmail.com>
2022-08-15 08:42:49 +0900

    lesson322

--- lesson32.py
+++ lesson32.py
@@ -2,3 +2,13 @@ l = ["good morning", "good afternoon", "good night"]
 
 for i in l:
     print(i)
+
+print("#########")
+
+def greeting():
+    yield "good morning"
+    yield "good afternoon"
+    yield "good night"
+
+for g in greeting():
+    print(g)
