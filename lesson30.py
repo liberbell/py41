da1d7075a4bcbf212dd36275f772c23823c54c3d
Liberbell <liberbell@gmail.com>
2022-08-14 14:06:57 +0900

    lesson3010

--- lesson30.py
+++ lesson30.py
@@ -28,15 +28,18 @@ def print_info(func):
         return result
     return wrapper
 
+@print_info
 def add_num(a, b):
     return a + b
 
+r = add_num(10, 20)
+print(r)
 # print("start")
 # r = add_num(10, 20)
 # print("end")
 
 # print(r)
 
-f = print_info(add_num)
-r = f(10, 20)
-print(r)
+# f = print_info(add_num)
+# r = f(10, 20)
+# print(r)
