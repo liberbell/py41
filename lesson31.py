13e9b62e225c273b937ed5c02629809e0bc0d027
Liberbell <liberbell@gmail.com>
2022-08-14 14:22:59 +0900

    lesson313

--- lesson31.py
+++ lesson31.py
@@ -3,3 +3,8 @@ l = ["Mon", "tue", "Wed", "Thu", "fri", "sat", "Sun"]
 def change_words(words, func):
     for word in words:
         print(func(word))
+
+def sample_func(word):
+    return word.capitalize()
+
+change_words(l, sample_func)
