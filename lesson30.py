1c0aa816c8a604bd61743434cb1066d3683b657c
Liberbell <liberbell@gmail.com>
2022-08-14 13:53:25 +0900

    lesson305

--- lesson30.py
+++ lesson30.py
@@ -8,14 +8,21 @@
 # r = f()
 # print(r)
 
-def circle_area_func(pi):
-    def circle_area(radius):
-        return pi * radius * radius
+# def circle_area_func(pi):
+#     def circle_area(radius):
+#         return pi * radius * radius
 
-    return circle_area
+#     return circle_area
 
-cal1 = circle_area_func(3.14)
-cal2 = circle_area_func(3.141592)
+# cal1 = circle_area_func(3.14)
+# cal2 = circle_area_func(3.141592)
 
-print(cal1(10))
-print(cal2(10))
+# print(cal1(10))
+# print(cal2(10))
+
+def add_num(a, b):
+    return a + b
+
+print("start")
+r = add_num(10, 20)
+print(r)
