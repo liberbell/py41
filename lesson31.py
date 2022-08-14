fc9bc26900de9f69e3278b9d89231025f3246b7c
Liberbell <liberbell@gmail.com>
2022-08-14 14:25:59 +0900

    lesson314

--- lesson31.py
+++ lesson31.py
@@ -4,7 +4,9 @@ def change_words(words, func):
     for word in words:
         print(func(word))
 
-def sample_func(word):
-    return word.capitalize()
+# def sample_func(word):
+#     return word.capitalize()
+
+sample_func = lambda word: word.capitalize()
 
 change_words(l, sample_func)
