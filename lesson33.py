a3d06777240c1e79366d0cb09da2f7b48d577d3c
Liberbell <liberbell@gmail.com>
2022-08-15 08:54:35 +0900

    lesson333

--- lesson33.py
+++ lesson33.py
@@ -2,9 +2,12 @@ l = (1, 2, 3, 4, 5)
 r = []
 
 for i in l:
-    r.append(i)
+    # r.append(i)
+    if i % 2 == 0:
+        r.append(i)
 
 print(r)
 
-r = [i for i in l]
+# r = [i for i in l]
+r = [i for i in l if i % 2 == 0]
 print(r)
