b9eed79c23f63f282aba23031669ddf8e255f539
Liberbell <liberbell@gmail.com>
2022-08-14 13:48:31 +0900

    lesson303

--- lesson30.py
+++ lesson30.py
@@ -1,8 +1,18 @@
-def outer(a, b):
-    def inner():
-        return a + b
+# def outer(a, b):
+#     def inner():
+#         return a + b
 
-    return inner
+#     return inner
 
-f = outer(1, 2)
-print(f)
+# f = outer(1, 2)
+# r = f()
+# print(r)
+
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+
+    return circle_area
+
+cal1 = circle_area_func(3.14)
+cal2 = circle_area_func(3.141592)
