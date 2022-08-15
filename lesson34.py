7f9c879d1ad22223d9dc0127d809ced5f2ef44a7
Liberbell <liberbell@gmail.com>
2022-08-16 08:32:30 +0900

    lesson344

--- lesson34.py
+++ lesson34.py
@@ -12,9 +12,10 @@ f = ["coffee", "milk", "water"]
 
 s = set()
 for i in range(10):
-    s.add(i)
+    if i % 2 == 0:
+        s.add(i)
 
 print(s)
 
-s = {i for i in range(10)}
+s = {i for i in range(10) if i % 2 == 0}
 print(s)
