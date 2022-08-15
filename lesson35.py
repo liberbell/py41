78651df89e55333bc769bbc48e2f14399c6766b3
Liberbell <liberbell@gmail.com>
2022-08-16 08:39:46 +0900

    lesson355

--- lesson35.py
+++ lesson35.py
@@ -1,12 +1,20 @@
-def g():
-    for i in range(10):
-        yield i
+# def g():
+#     for i in range(10):
+#         yield i
 
-g = g()
-print(type(g))
-g = (i for i in range(10) if i % 2 == 0)
+# g = g()
+# print(type(g))
+# g = (i for i in range(10) if i % 2 == 0)
 # print(type(g))
 # print(g)
 
-for x in g:
-    print(x)
+# for x in g:
+#     print(x)
+
+animal = "cat"
+
+def f():
+    print(animal)
+
+print(animal)
+f()
