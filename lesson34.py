8296ad3141da87f554acdac37b75099e8261d3fc
Liberbell <liberbell@gmail.com>
2022-08-15 09:03:13 +0900

    lesson343

--- lesson34.py
+++ lesson34.py
@@ -4,8 +4,11 @@ from tkinter import Y
 w = ["mon", "tue", "wed"]
 f = ["coffee", "milk", "water"]
 
-d = {}
-for k, v in zip(w, f):
-    d[k] = v
+# d = {}
+# for k, v in zip(w, f):
+#     d[k] = v
 
+# print(d)
+
+d = {x: y for x, y in zip(w, f)}
 print(d)
