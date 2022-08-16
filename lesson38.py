22651d4a304cf982d566af2b8a2f2c74aec2a0a2
Liberbell <liberbell@gmail.com>
2022-08-17 08:53:41 +0900

    lesson383

--- lesson38.py
+++ lesson38.py
@@ -1,7 +1,11 @@
 import sys
+import lesson_package.utils
 
-l = sys.argv
-print(l[1])
+# l = sys.argv
+# print(l[1])
 
-for i in l:
-    print(i)
+# for i in l:
+#     print(i)
+
+r = lesson_package.utils.say_twice("hello")
+print(r)
