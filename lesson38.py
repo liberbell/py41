309b0744179898b3fbb14cf9340463e184691767
Liberbell <liberbell@gmail.com>
2022-08-17 08:55:46 +0900

    lesson384

--- lesson38.py
+++ lesson38.py
@@ -1,6 +1,7 @@
 import sys
-import lesson_package.utils
-from lesson_package import utils
+# import lesson_package.utils
+# from lesson_package import utils
+from lesson_package.utils import say_twice
 
 # l = sys.argv
 # print(l[1])
@@ -9,5 +10,6 @@ from lesson_package import utils
 #     print(i)
 
 # r = lesson_package.utils.say_twice("hello")
-r = utils.say_twice("bye")
+# r = utils.say_twice("bye")
+r = say_twice("sleepy")
 print(r)
