507213f7df747e371d20a8bc177b141598420db8
Liberbell <liberbell@gmail.com>
2022-08-17 08:31:34 +0900

    lesson362

--- lesson36.py
+++ lesson36.py
@@ -1,10 +1,13 @@
 l = [1, 2, 3]
 i = 5
-del l
+# del l
 
 try:
+    () + 1
     print(l[i])
 except IndexError as exc:
     print("don`t worry. {}".format(exc))
 except NameError as exc:
     print("name error. {}".format(exc))
+except Exception as exc:
+    print("Exception error.{}".format(exc))
