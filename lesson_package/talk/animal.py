0bb5a7eda560934c259e67a33ad847e635cbf3af
Liberbell <liberbell@gmail.com>
2022-08-20 11:00:12 +0900

    animal2

--- lesson_package/talk/animal.py
+++ lesson_package/talk/animal.py
@@ -1,5 +1,3 @@
-from ast import main
-from tkinter.tix import MAIN
 from lesson_package.tools import utils
 
 def sing():
@@ -8,6 +6,6 @@ def sing():
 def cry():
     return utils.say_twice("gdfsdfdsr2")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(sing())
     print("animal", __name__)
