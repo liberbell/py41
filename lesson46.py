b8ea731c468756e0b277a3a35473e3953ad85972
Liberbell <liberbell@gmail.com>
2022-08-24 08:19:22 +0900

    lesson461

--- lesson46.py
+++ lesson46.py
@@ -8,5 +8,12 @@ class Word(object):
     def __len__(self):
         return len(self.text)
 
+    def __add__(self):
+        self.text.lower() + word.text.lower()
+
 w = Word("test")
 print(len(w))
+w2 = "######"
+
+w + w2
+print(w + w2)
