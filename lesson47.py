b65b2e57dedaa3fe270e9914461388fb91ba711c
Liberbell <liberbell@gmail.com>
2022-08-24 08:36:27 +0900

    lesson476

--- lesson47.py
+++ lesson47.py
@@ -1,5 +1,9 @@
-f = open("test.txt", "w")
-f.write("TEXT insert\n")
+# f = open("test.txt", "w")
+# f.write("TEXT insert\n")
 # print("I am print", file=f)
 # print("My", "name", "is", "Bob", sep="#", end="!", file=f)
-f.close()
+# f.close()
+
+with open("test.txt", "w") as f:
+    f.write("Text insert\n")
+    print("I am print", file=f)
