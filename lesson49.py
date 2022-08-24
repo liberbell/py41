5b3726f1b56322885d615e28bb19697acbdc847b
Liberbell <liberbell@gmail.com>
2022-08-25 08:59:37 +0900

    lesson493

--- lesson49.py
+++ lesson49.py
@@ -4,3 +4,4 @@ with open("test.csv", "w")as csv_file:
     fieldnames = ["Name", "Count"]
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
+    writer.writerow({"Name": "A", "Count": "1"})
