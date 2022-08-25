fbf8df1759de185c6a300659094a5b92df19a969
Liberbell <liberbell@gmail.com>
2022-08-26 08:36:57 +0900

    lesson505

--- lesson50.py
+++ lesson50.py
@@ -3,4 +3,7 @@ import os
 print(os.path.exists("test.txt"))
 print(os.path.isfile("test.txt"))
 print(os.path.isdir("design"))
-os.rename("rename.txt", "test.txt")
+# os.rename("rename.txt", "test.txt")
+
+os.mkdir("test_dir")
+os.rmdir("test_dir")
