29e6c3a37e0a08dbdcf12c94d44787449133aee7
Liberbell <liberbell@gmail.com>
2022-08-26 08:38:32 +0900

    lesson506

--- lesson50.py
+++ lesson50.py
@@ -1,9 +1,12 @@
 import os
+import pathlib
 
 print(os.path.exists("test.txt"))
 print(os.path.isfile("test.txt"))
 print(os.path.isdir("design"))
 # os.rename("rename.txt", "test.txt")
 
-os.mkdir("test_dir")
-os.rmdir("test_dir")
+# os.mkdir("test_dir")
+# os.rmdir("test_dir")
+
+pathlib.Path("empty.txt").touch()
