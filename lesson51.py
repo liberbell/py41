9867e861bfb246cdfcc1d14de5810682ed8e70a1
Liberbell <liberbell@gmail.com>
2022-08-27 10:29:36 +0900

    lesson519

--- lesson51.py
+++ lesson51.py
@@ -1,5 +1,6 @@
 # import tarfile
 import zipfile
+import glob
 
 # with tarfile.open("test.txt.gz", "w:gz") as tr:
 #     tr.add("test_dir")
@@ -10,5 +11,7 @@ import zipfile
 #         print(f.read())
 
 with zipfile.ZipFile("test.zip", "w") as zip_file:
-    zip_file.write("test_dir")
-    zip_file.write("test_dir/test.txt")
+    # zip_file.write("test_dir")
+    # zip_file.write("test_dir/test.txt")
+    for f in glob.glob("test.dir/**", recursive=True):
+        print(f)
