3c60f8814f2862b32a552cffd2ae8303d271f36b
Liberbell <liberbell@gmail.com>
2022-08-27 10:22:56 +0900

    lesson515

--- lesson51.py
+++ lesson51.py
@@ -4,4 +4,6 @@ import tarfile
 #     tr.add("test_dir")
 
 with tarfile.open("test.txt.gz", "r:gz") as tr:
-    tr.extractall(path="test_tar")
+    # tr.extractall(path="test_tar")
+    with tr.extractfile("test_dir/sub_dir/sub.txt") as f:
+        print(f.read())
