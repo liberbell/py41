dded5114e2d4cbcec80435adb4ddbf1420ecc651
Liberbell <liberbell@gmail.com>
2022-08-29 08:41:16 +0900

    main3

--- roboter/main.py
+++ roboter/main.py
@@ -1,5 +1,10 @@
 import os
+import csv
 
 name_input = input("Hello. I am Roboko. What is your name?")
 recom_input = input("{}, Which restaurants do you like?".format(name_input))
 print(recom_input)
+
+with open("ranking.csv", "w") as rank_csv:
+    fieldnames = ["Name", "Count"]
+    writer = csv.DictWriter(rank_csv, fieldnames=fieldnames)
