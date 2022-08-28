cfbae577aada40379328e04687605e8b7074fb3e
Liberbell <liberbell@gmail.com>
2022-08-29 08:38:26 +0900

    main2

--- roboter/main.py
+++ roboter/main.py
@@ -1,4 +1,5 @@
 import os
 
-user_input = input("Hello. I am Roboko. What is your name?")
-print(user_input)
+name_input = input("Hello. I am Roboko. What is your name?")
+recom_input = input("{}, Which restaurants do you like?".format(name_input))
+print(recom_input)
