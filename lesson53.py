819ea157bf51723d68898c2a9b21d575189c881c
Liberbell <liberbell@gmail.com>
2022-08-28 15:05:56 +0900

    lesson534

--- lesson53.py
+++ lesson53.py
@@ -1,5 +1,8 @@
 import subprocess
 # import os
 
-subprocess.run("ls -al", shell=True)
+# subprocess.run("ls -al", shell=True)
 # os.system("ls")
+r = subprocess.run("lsa", shell=True)
+print(r.returncode())
+print("#######")
