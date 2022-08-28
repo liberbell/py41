0803ad96ed826e815c405e96ccf9e2d049dc9afe
Liberbell <liberbell@gmail.com>
2022-08-28 15:09:58 +0900

    lesson534

--- lesson53.py
+++ lesson53.py
@@ -4,5 +4,12 @@ import subprocess
 # subprocess.run("ls -al", shell=True)
 # os.system("ls")
 r = subprocess.run("lsa", shell=True)
-print(r.returncode())
+# print(r.returncode)
+
 print("#######")
+
+p1 = subprocess.Popen(["ls", "-al"], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(["grep", "test"], stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0]
+print(output)
