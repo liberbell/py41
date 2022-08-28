b77c0f19ab6235e098db9ecded4ccb051dafe88a
Liberbell <liberbell@gmail.com>
2022-08-28 14:53:22 +0900

    lesson521

--- lesson52.py
+++ lesson52.py
@@ -4,3 +4,10 @@ with tempfile.TemporaryFile(mode="w+") as temp:
     temp.write("hello")
     temp.seek(0)
     print(temp.read())
+
+with tempfile.NamedTemporaryFile(delete=False):
+    print(t.name)
+    with open(t.name, "w+") as f:
+        f.write("test\n")
+        f.seek(0)
+        print(f.read())
