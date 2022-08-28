f8b30b02a5c81160ef1125ed1490f6b8fbdf5df8
Liberbell <liberbell@gmail.com>
2022-08-28 15:21:08 +0900

    lesson544

--- lesson54.py
+++ lesson54.py
@@ -9,3 +9,8 @@ today = datetime.date.today()
 print(today)
 print(today.isoformat())
 print(today.strftime("%d/%m/%y"))
+
+t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(t)
+print(t.isoformat())
+print(t.strftime("%H%M%S%f"))
