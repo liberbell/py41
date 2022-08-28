eb209d48b5b43ed61966a640577b964a3ba28b2c
Liberbell <liberbell@gmail.com>
2022-08-28 15:22:57 +0900

    lesson545

--- lesson54.py
+++ lesson54.py
@@ -13,4 +13,7 @@ print(today.strftime("%d/%m/%y"))
 t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
 print(t)
 print(t.isoformat())
-print(t.strftime("%H%M%S%f"))
+print(t.strftime("%H_%M_%S_%f"))
+
+d = datetime.timedelta(weeks=-1)
+print(now + d)
