a9fe38031e7f2319dbf56eda4a2962e4bcd7b0f9
Liberbell <liberbell@gmail.com>
2022-08-30 08:31:06 +0900

    lesson555

--- lesson55.py
+++ lesson55.py
@@ -7,14 +7,14 @@ tree = ET.ElementTree(element=root)
 employee = ET.SubElement(root, "employee")
 employ = ET.SubElement(employee, "employ")
 
-empoly_id = ET.SubElement(employ, id)
+empoly_id = ET.SubElement(employ, "id")
 empoly_id.text = "111"
-empoly_id = ET.SubElement(employ, name)
+empoly_id = ET.SubElement(employ, "name")
 empoly_id.text = "Eric"
 
-empoly_id = ET.SubElement(employ, id)
+empoly_id = ET.SubElement(employ, "id")
 empoly_id.text = "222"
-empoly_id = ET.SubElement(employ, name)
+empoly_id = ET.SubElement(employ, "name")
 empoly_id.text = "Alex"
 
 tree.write("test.xml", encoding=utf_8, xml_declaration=True) 
