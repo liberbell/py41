8e52e9dcd8cc12df4ffbde71d4c34bd71247c57f
Liberbell <liberbell@gmail.com>
2022-08-30 08:40:11 +0900

    lesson564

--- lesson56.py
+++ lesson56.py
@@ -12,5 +12,8 @@ print(j)
 print("####")
 print(json.dumps(j))
 
-with open("test.json", "w") as json_file:
-    json.dump(j, json_file)
+# with open("test.json", "w") as json_file:
+#     json.dump(j, json_file)
+
+with open("test.json", "r") as json_file:
+    print(json.load(json_file))
