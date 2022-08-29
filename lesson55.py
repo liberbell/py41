ac9c26d8b3d484e6f77a40ff1b8ffcaf0473be02
Liberbell <liberbell@gmail.com>
2022-08-30 08:26:16 +0900

    lesson552

--- lesson55.py
+++ lesson55.py
@@ -1,2 +1,7 @@
 import xml.etree.ElementTree as ET
 
+root = ET.Element("root")
+tree = ET.ElementTree(element=root)
+
+employee = ET.SubElement(root, "employee")
+empoly = ET.SubElement(employee, "employ")
