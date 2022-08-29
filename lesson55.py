d975048ba38a2a3ef9c5c2886403d1284c284f98
Liberbell <liberbell@gmail.com>
2022-08-30 08:30:05 +0900

    lesson554

--- lesson55.py
+++ lesson55.py
@@ -1,3 +1,4 @@
+from encodings import utf_8
 import xml.etree.ElementTree as ET
 
 root = ET.Element("root")
@@ -7,3 +8,13 @@ employee = ET.SubElement(root, "employee")
 employ = ET.SubElement(employee, "employ")
 
 empoly_id = ET.SubElement(employ, id)
+empoly_id.text = "111"
+empoly_id = ET.SubElement(employ, name)
+empoly_id.text = "Eric"
+
+empoly_id = ET.SubElement(employ, id)
+empoly_id.text = "222"
+empoly_id = ET.SubElement(employ, name)
+empoly_id.text = "Alex"
+
+tree.write("test.xml", encoding=utf_8, xml_declaration=True) 
