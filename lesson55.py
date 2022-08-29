9ccddf012ec2fc8f64c76abdb78934fbc5cffeea
Liberbell <liberbell@gmail.com>
2022-08-30 08:34:11 +0900

    lesson558

--- lesson55.py
+++ lesson55.py
@@ -1,20 +1,28 @@
 from encodings import utf_8
 import xml.etree.ElementTree as ET
 
-root = ET.Element("root")
-tree = ET.ElementTree(element=root)
+# root = ET.Element("root")
+# tree = ET.ElementTree(element=root)
 
-employee = ET.SubElement(root, "employee")
-employ = ET.SubElement(employee, "employ")
+# employee = ET.SubElement(root, "employee")
+# employ = ET.SubElement(employee, "employ")
 
-empoly_id = ET.SubElement(employ, "id")
-empoly_id.text = "111"
-empoly_id = ET.SubElement(employ, "name")
-empoly_id.text = "Eric"
+# empoly_id = ET.SubElement(employ, "id")
+# empoly_id.text = "111"
+# empoly_id = ET.SubElement(employ, "name")
+# empoly_id.text = "Eric"
 
-empoly_id = ET.SubElement(employ, "id")
-empoly_id.text = "222"
-empoly_id = ET.SubElement(employ, "name")
-empoly_id.text = "Alex"
+# empoly_id = ET.SubElement(employ, "id")
+# empoly_id.text = "222"
+# empoly_id = ET.SubElement(employ, "name")
+# empoly_id.text = "Alex"
 
-tree.write("test.xml", encoding="utf_8", xml_declaration=True) 
+# tree.write("test.xml", encoding="utf_8", xml_declaration=True) 
+
+tree = ET.ElementTree(file="test.xml")
+
+root = tree.getroot()
+for employee in root:
+    for employ in employee:
+        for person in employ:
+            print(person.tag, person.text)
