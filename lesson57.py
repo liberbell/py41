61a89d7949afa305544bb66dc5c6163ff0bc2a9d
Liberbell <liberbell@gmail.com>
2022-08-30 08:50:19 +0900

    lesson575

--- lesson57.py
+++ lesson57.py
@@ -1,8 +1,10 @@
 import urllib.request
 import json
 
-url = "https://httpbin.org/get"
-url2 = "http://httpbin.org/get"
+url = "https://httpbin.org/get" + "?" + "?"
+url2 = "http://httpbin.org/get" + "?" + "?"
+
+payloads = {"key1": "value1", "key2": "value2"}
 
 # with urllib.request.urlopen(url) as url_open:
 #     print(url_open.read().decode("utf-8"))
@@ -10,4 +12,4 @@ url2 = "http://httpbin.org/get"
 
 with urllib.request.urlopen(url2) as url_open:
     print(url_open.read().decode("utf-8"))
-    print(json.loads(url_open.read().decode("utf-8")))
+    # print(json.loads(url_open.read().decode("utf-8")))
