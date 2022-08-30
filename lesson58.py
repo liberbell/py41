d131c940d4b63acb07cb829bc8511a63a5ecb056
Liberbell <liberbell@gmail.com>
2022-08-31 08:42:41 +0900

    lesson583

--- lesson58.py
+++ lesson58.py
@@ -6,7 +6,7 @@ url1 = "https://httpbin.org/get"
 url2 = "https://httpbin.org/post"
 
 # r = requests.get(url1, params=payload)
-r = requests.post(url1, data=payload)
+r = requests.post(url2, data=payload)
 
 print(r.status_code)
 print(r.text)
