109f71d32492aaba3351894da407e176f9425c8c
Liberbell <liberbell@gmail.com>
2022-08-31 08:42:13 +0900

    lesson583

--- lesson58.py
+++ lesson58.py
@@ -3,11 +3,11 @@ import requests
 payload = {"key1": "value1", "key2": "value2"}
 
 url1 = "https://httpbin.org/get"
-url1 = "https://httpbin.org/get"
+url2 = "https://httpbin.org/post"
 
 # r = requests.get(url1, params=payload)
 r = requests.post(url1, data=payload)
 
 print(r.status_code)
 print(r.text)
-# print(r.json())
+print(r.json())
