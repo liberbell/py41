17903ec7e7faec0edcb477f5bc6db8ac4075b886
Liberbell <liberbell@gmail.com>
2022-08-31 08:32:09 +0900

    lesson572

--- lesson57.py
+++ lesson57.py
@@ -6,10 +6,15 @@ payloads = {"key1": "value1", "key2": "value2"}
 url = "https://httpbin.org/get" + "?" + urllib.parse.urlencode(payloads)
 url2 = "http://httpbin.org/get" + "?" + "?"
 
+print(url)
+
 # with urllib.request.urlopen(url) as url_open:
 #     print(url_open.read().decode("utf-8"))
 #     print(json.loads(url_open.read().decode("utf-8")))
 
-with urllib.request.urlopen(url2) as url_open:
-    print(url_open.read().decode("utf-8"))
+# with urllib.request.urlopen(url2) as url_open:
+#     print(url_open.read().decode("utf-8"))
     # print(json.loads(url_open.read().decode("utf-8")))
+
+payloads = json.dump(payloads).encode("utf-8")
+req = urllib.request.Request("https://httpbin.org/post", data=payloads, method="POST")
