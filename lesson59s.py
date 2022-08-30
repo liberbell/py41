322e472e122d0f6f882ba5fc0869778fbfd18249
Liberbell <liberbell@gmail.com>
2022-08-31 08:51:30 +0900

    lesson59s2

--- lesson59s.py
+++ lesson59s.py
@@ -2,3 +2,12 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind("127.0.0.1", 50008)
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print("Data: {}, Addr: {}".format(data, addr))
