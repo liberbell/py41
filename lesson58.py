ac0c93f445c175c02fdd34f79392d9bedd7d9a64
Liberbell <liberbell@gmail.com>
2022-08-31 08:43:21 +0900

    lesson583

--- lesson58.py
+++ lesson58.py
@@ -4,9 +4,11 @@ payload = {"key1": "value1", "key2": "value2"}
 
 url1 = "https://httpbin.org/get"
 url2 = "https://httpbin.org/post"
+url3 = "https://httpbin.org/put"
 
 # r = requests.get(url1, params=payload)
-r = requests.post(url2, data=payload)
+# r = requests.post(url2, data=payload)
+r = requests.put(url3, data=payload)
 
 print(r.status_code)
 print(r.text)
