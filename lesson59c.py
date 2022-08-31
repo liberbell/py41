764d1382c61c74effb8fe992d182266bb736dbb8
Liberbell <liberbell@gmail.com>
2022-09-01 08:32:50 +0900

    lesson59c3

--- lesson59c.py
+++ lesson59c.py
@@ -1,7 +1,7 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
-    c.bind("127.0.0.1", 50008)
+    c.bind(("127.0.0.1", 50008))
     c.sendall(b"hello")
     data = c.recv(1024)
     print(repr(data))
