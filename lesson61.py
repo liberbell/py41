b4f1921ab4082f15dca3f0192d3464da2038b754
Liberbell <liberbell@gmail.com>
2022-09-02 08:44:25 +0900

    lesson6111

--- lesson61.py
+++ lesson61.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import request
 from flask import Response
 
+
 app = Flask(__name__)
 
 # @app.route("/")
@@ -17,7 +18,9 @@ def hello_world():
 @app.route("/hello")
 @app.route("/hello/<username>")
 def hello_world2(username=None):
-    return "<p>Hello world! {}</p>".format(username)
+    # return "<p>Hello world! {}</p>".format(username)
+    return render_template("hello.html", username=username)
+
 
 def main():
     app.debug = True
