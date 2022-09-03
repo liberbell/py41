f96041aaeb6a77971fc9ccedb11b8f2ff86dfba2
Liberbell <liberbell@gmail.com>
2022-09-03 14:41:43 +0900

    lesson611

--- lesson61.py
+++ lesson61.py
@@ -1,4 +1,5 @@
 from crypt import methods
+import sqlite3
 from flask import Flask
 from flask import g
 from flask import render_template
@@ -11,6 +12,12 @@ app = Flask(__name__)
 # @app.route("/")
 # def hello_world():
 #     return "<p>Hello world!</p>"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect("test_sqlite.db")
+    return db
 
 @app.route('/top')
 def hello_world():
