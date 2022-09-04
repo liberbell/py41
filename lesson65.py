f8737857f067b1c527ab6748f04bae056d485185
Liberbell <liberbell@gmail.com>
2022-09-05 08:49:58 +0900

    lesson651

--- lesson65.py
+++ lesson65.py
@@ -1,6 +1,13 @@
+from pydoc import doc
+
+
 class Cal(object):
 
     def add_num_and_double(self, x, y):
         result = x + y
         result *= 2
         return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
