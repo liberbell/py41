53d76a93178b27bbed2299d4cd16f9c9d9346efd
Liberbell <liberbell@gmail.com>
2022-09-04 20:58:20 +0900

    test_flask3

--- test_flask.py
+++ test_flask.py
@@ -3,9 +3,11 @@ import requests
 r = requests.post(
     "http://127.0.0.1:5000/employee", data={"name": "Alex"}
 )
+print(r.text)
 
 r = requests.put(
     "http://127.0.0.1:5000/employee", data={"name": "Alex", "new_name": "Alexander"}
 )
+print(r.text)
 
 print(r.text)
