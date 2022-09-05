7c02997325317fdbe9d5505b655c57241cf31e7c
Liberbell <liberbell@gmail.com>
2022-09-06 08:50:12 +0900

    test_calculation3

--- test_calculation.py
+++ test_calculation.py
@@ -2,6 +2,10 @@ import unittest
 import calculation
 
 class CalTest(unittest.TestCase):
+    def setup(self):
+        print("setup")
+        self.cal = calculation.Cal()
+        
     def test_add_num_and_double(self):
         cal = calculation.Cal()
         self.assertEqual(cal.add_num_and_double(1, 1), 4)
