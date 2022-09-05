6500de971cac87b68274606cf42be3aef7b87502
Liberbell <liberbell@gmail.com>
2022-09-06 08:31:25 +0900

    lesson652

--- lesson65.py
+++ lesson65.py
@@ -9,11 +9,13 @@ class Cal(object):
         4
 
         >>> c.add_num_and_double("1", "1")
+        Traceback (most recent call last)
+        ...
+        ValueError
         4
         """
         if type(x) is not int or type(y) is not int:
             raise ValueError
-    # def add_num_and_double(self, x, y):
         result = x + y
         result *= 2
         return result
