e6397cd1a917414b313b4cf1f165c9a38125dd82
Liberbell <liberbell@gmail.com>
2022-09-06 08:29:23 +0900

    lesson651

--- lesson65.py
+++ lesson65.py
@@ -2,17 +2,18 @@ from pydoc import doc
 
 
 class Cal(object):
-    """Add and double
-    >>> c = Cal()
-    >>> c.add_num_and_double(1, 1)
-    4
-
-    >>> c.add_num_and_double("1", "1")
-    4
-    """
-    if type(x) is not int or type(y) is not int:
-        raise ValueError
     def add_num_and_double(self, x, y):
+        """Add and double
+        >>> c = Cal()
+        >>> c.add_num_and_double(1, 1)
+        4
+
+        >>> c.add_num_and_double("1", "1")
+        4
+        """
+        if type(x) is not int or type(y) is not int:
+            raise ValueError
+    # def add_num_and_double(self, x, y):
         result = x + y
         result *= 2
         return result
