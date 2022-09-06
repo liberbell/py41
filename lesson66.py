14e4f4a0c1febe1cf56e8fd73bf53c7f38ccc268
Liberbell <liberbell@gmail.com>
2022-09-07 08:36:25 +0900

    lesson6610

--- lesson66.py
+++ lesson66.py
@@ -3,6 +3,11 @@ import calculation
 
 
 class TestCal(object):
+    
+    def setup_method(self, method):
+        print("Method: {}".format(method.__name__))
+        self.cal = calculation.Cal()
+
     def test_add_num_and_double(self):
         cal = calculation.Cal()
         assert cal.add_num_and_double(1, 1) == 4
@@ -10,4 +15,4 @@ class TestCal(object):
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
             cal = calculation.Cal()
-            cal.add_num_and_double("1", "1")
+            cal.add_num_and_double(1, 1)
