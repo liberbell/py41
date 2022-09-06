ddf5dbcc149074a7edaf27a7cbede45ece2a1bef
Liberbell <liberbell@gmail.com>
2022-09-06 09:02:32 +0900

    lesson663

--- lesson66.py
+++ lesson66.py
@@ -3,3 +3,6 @@ import calculation
 def test_add_num_and_double():
     cal = calculation.Cal()
     assert cal.add_num_and_double(1, 1) == 4
+
+if __name__ == "__main__":
+    test_add_num_and_double.main()
