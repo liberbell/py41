c61636d948df982c834ebad8fe27bf415bde52d5
Liberbell <liberbell@gmail.com>
2022-09-07 08:27:38 +0900

    lesson662

--- lesson66.py
+++ lesson66.py
@@ -14,3 +14,6 @@ class TestCal(object):
     def test_add_num_and_double_raise(self):
         with self.assertRaise(ValueError):
             self.cal.add_num_and_doubule("1", "1")
+
+if __name__ == "__main__":
+    TestCal.main()
