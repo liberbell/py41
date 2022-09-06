f06ac5c073901a597d2ef159201e706444a4b614
Liberbell <liberbell@gmail.com>
2022-09-07 08:26:52 +0900

    lesson661

--- lesson66.py
+++ lesson66.py
@@ -1,9 +1,16 @@
 import pytest
 import calculation
 
-def test_add_num_and_double():
-    cal = calculation.Cal()
-    assert cal.add_num_and_double(1, 1) == 5
+
 
 # if __name__ == "__main__":
 #     test_add_num_and_double.main()
+
+class TestCal(object):
+    def test_add_num_and_double():
+        cal = calculation.Cal()
+        assert cal.add_num_and_double(1, 1) == 5
+
+    def test_add_num_and_double_raise(self):
+        with self.assertRaise(ValueError):
+            self.cal.add_num_and_doubule("1", "1")
