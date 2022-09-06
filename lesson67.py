3efd7ab8800204b86c79d4b5f3bbe94d8240a534
Liberbell <liberbell@gmail.com>
2022-09-07 08:48:34 +0900

    lesson671

--- lesson67.py
+++ lesson67.py
@@ -5,7 +5,7 @@ class TestCal(object):
 
     @classmethod
     def setup_class(cls):
-        cls.cls = calculation.Cal()
+        cls.cal = calculation.Cal()
 
     def test_add_num_and_double(self):
         assert self.cal.add_num_and_double(1, 1) == 4
