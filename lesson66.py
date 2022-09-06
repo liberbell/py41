04a023d253f620841bdaff377cdc95ca63636818
Liberbell <liberbell@gmail.com>
2022-09-07 08:33:44 +0900

    lesson669

--- lesson66.py
+++ lesson66.py
@@ -10,4 +10,4 @@ class TestCal(object):
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
             cal = calculation.Cal()
-            cal.add_num_and_doubule("1", "1")
+            cal.add_num_and_double("1", "1")
