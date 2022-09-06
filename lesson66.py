ef444569c8ad5be36762e2de1ae4564ae6bb3525
Liberbell <liberbell@gmail.com>
2022-09-07 08:38:13 +0900

    lesson6612

--- lesson66.py
+++ lesson66.py
@@ -13,10 +13,8 @@ class TestCal(object):
         del self.cal
 
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1, 1) == 4
+        self.cal.add_num_and_double(1, 1) == 4
 
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
-            cal = calculation.Cal()
-            cal.add_num_and_double(1, 1)
+            self.cal.add_num_and_double(1, 1)
