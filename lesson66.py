e3a2397f5b0014387ee90c102d37f4ebba83a410
Liberbell <liberbell@gmail.com>
2022-09-07 08:31:08 +0900

    lesson665

--- lesson66.py
+++ lesson66.py
@@ -2,14 +2,10 @@ import pytest
 import calculation
 
 
-
-# if __name__ == "__main__":
-#     test_add_num_and_double.main()
-
 class TestCal(object):
     def test_add_num_and_double():
         cal = calculation.Cal()
-        assert cal.add_num_and_double(1, 1) == 5
+        assert cal.add_num_and_double(1, 1) == 4
 
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
