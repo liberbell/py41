5bdba3eecd4c8e76cff5b86a2fe9fb5fa976fc8b
Liberbell <liberbell@gmail.com>
2022-09-08 08:41:34 +0900

    lesson676

--- lesson67.py
+++ lesson67.py
@@ -6,6 +6,7 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = calculation.Cal()
+        cls.test_file_name = "test1.txt"
 
     def test_add_num_and_double(self):
         # os_name = request.config.getoption("--os-name")
@@ -15,3 +16,6 @@ class TestCal(object):
         # elif os_name == "windows":
         #     print("dir")
         assert self.cal.add_num_and_double(1, 1) == 4
+
+    def test_save(self, tmpdir):
+
