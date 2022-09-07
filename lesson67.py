7049602553762525356af47d41d7bc3221260d3a
Liberbell <liberbell@gmail.com>
2022-09-08 08:36:44 +0900

    lesson675

--- lesson67.py
+++ lesson67.py
@@ -7,11 +7,12 @@ class TestCal(object):
     def setup_class(cls):
         cls.cal = calculation.Cal()
 
-    def test_add_num_and_double(self, request):
-        os_name = request.config.getoption("--os-name")
-        print(os_name)
-        if os_name == "mac":
-            print("ls")
-        elif os_name == "windows":
-            print("dir")
+    def test_add_num_and_double(self, tmpdir):
+        # os_name = request.config.getoption("--os-name")
+        # print(os_name)
+        # if os_name == "mac":
+        #     print("ls")
+        # elif os_name == "windows":
+        #     print("dir")
+        print(tmpdir)
         assert self.cal.add_num_and_double(1, 1) == 4
