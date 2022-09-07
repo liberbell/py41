6241a1730338b2333e381878bfcc34c0841ab394
Liberbell <liberbell@gmail.com>
2022-09-08 08:39:27 +0900

    calculation2

--- calculation.py
+++ calculation.py
@@ -11,3 +11,5 @@ class Cal(object):
 
     def save(self, dir_path, file_name):
         if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+        file_path = os.path.join(dir_path, file_name)
