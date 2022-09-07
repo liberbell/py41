0509e5ef076dc8c88e69151a39f6b125ea501940
Liberbell <liberbell@gmail.com>
2022-09-08 08:53:33 +0900

    lesson678

--- lesson67.py
+++ lesson67.py
@@ -21,6 +21,7 @@ class TestCal(object):
 
     def test_save(self, tmpdir):
         print(self.test_file_name)
+        print(tempdir)
         self.cal.save(tmpdir, self.test_file_name)
         test_file_path = os.path.join(tempdir, self.test_file_name)
         assert os.path.exists(test_file_path) is True
