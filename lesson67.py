2f3aa4c4d67b9f1a12d16ec8e53f24f832971ff0
Liberbell <liberbell@gmail.com>
2022-09-08 08:25:16 +0900

    lesson671

--- lesson67.py
+++ lesson67.py
@@ -8,4 +8,8 @@ class TestCal(object):
         cls.cal = calculation.Cal()
 
     def test_add_num_and_double(self):
+        if os_name == "mac":
+            print("ls")
+        elif os_name = "windows":
+            print("dir")
         assert self.cal.add_num_and_double(1, 1) == 4
