22dafda0835472a9f78a48b3186f1387bfc4cb9f
Liberbell <liberbell@gmail.com>
2022-09-09 08:31:47 +0900

    conftest1

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
-from email.policy import default
+import pytest
 
+@pytest.fixture()
+def csv_file():
+    return "csv_file!!!"
 
 def pytest_addoption(parser):
     parser.addoption("--os-name", default="linux", help="os name")
