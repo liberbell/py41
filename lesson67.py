ebeda4b7fc911f468866534deeed09cfd4527a11
Liberbell <liberbell@gmail.com>
2022-09-09 08:33:34 +0900

    lesson672

--- lesson67.py
+++ lesson67.py
@@ -10,13 +10,14 @@ class TestCal(object):
         cls.cal = calculation.Cal()
         cls.test_file_name = "test1.txt"
 
-    def test_add_num_and_double(self):
+    def test_add_num_and_double(self, csv_file):
         # os_name = request.config.getoption("--os-name")
         # print(os_name)
         # if os_name == "mac":
         #     print("ls")
         # elif os_name == "windows":
         #     print("dir")
+        print(csv_file)
         assert self.cal.add_num_and_double(1, 1) == 4
 
     def test_save(self, tmpdir):
