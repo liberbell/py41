32dd2b8560d0b6fdd04ec32efb6ed81d60dafd7b
Liberbell <liberbell@gmail.com>
2022-09-09 08:35:47 +0900

    conftest3

--- conftest.py
+++ conftest.py
@@ -3,7 +3,9 @@ import pytest
 @pytest.fixture()
 def csv_file():
     with open("test.csv", "w+") as c:
+        print("before test")
         yield c
+        print("after test")
     # yield "csv_file!!!"
 
 def pytest_addoption(parser):
