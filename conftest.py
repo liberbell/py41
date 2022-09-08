01f937d82cc01fe6cc5800351179e1f6f0e7f1cb
Liberbell <liberbell@gmail.com>
2022-09-09 08:34:08 +0900

    conftest2

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture()
 def csv_file():
-    return "csv_file!!!"
+    yield "csv_file!!!"
 
 def pytest_addoption(parser):
     parser.addoption("--os-name", default="linux", help="os name")
