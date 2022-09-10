d267bafb33694479dcf7968b7104cc2c1d71248c
Liberbell <liberbell@gmail.com>
2022-09-10 10:31:36 +0900

    lesson6814

--- lesson68.py
+++ lesson68.py
@@ -18,10 +18,15 @@ def worker2():
     logging.debug("end")
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=worker1)
-    t1.setDaemon(True)
-    t2 = threading.Thread(target=worker2)
-    t1.start()
-    t2.start()
-    print("started")
-    t1.join()
+    threads = []
+    for _ in range(5):
+        t = threading.Thread(target=worker1)
+        t.setDaemon()
+        t.start()
+    # t1 = threading.Thread(target=worker1)
+    # t1.setDaemon(True)
+    # t2 = threading.Thread(target=worker2)
+    # t1.start()
+    # t2.start()
+    # print("started")
+    # t1.join()
