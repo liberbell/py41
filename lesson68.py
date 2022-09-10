dedf0ed0f9f32c68d7d63fbfaae0c3e9cbc52018
Liberbell <liberbell@gmail.com>
2022-09-10 10:27:37 +0900

    lesson6812

--- lesson68.py
+++ lesson68.py
@@ -9,12 +9,12 @@ logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
 
 def worker1():
     logging.debug("start")
-    time.sleep(0.5)
+    time.sleep(5)
     logging.debug("end")
 
 def worker2():
     logging.debug("start")
-    time.sleep(0.5)
+    time.sleep(2)
     logging.debug("end")
 
 if __name__ == "__main__":
