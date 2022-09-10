a9621854866533f3f01084c7b061b25c741bd070
Liberbell <liberbell@gmail.com>
2022-09-10 10:09:28 +0900

    lesson682

--- lesson68.py
+++ lesson68.py
@@ -3,3 +3,14 @@ import threading
 def worker1():
     print(threading.currentThread().getName(), "start")
     print(threading.currentThread().getName(), "end")
+
+def worker2():
+    print(threading.currentThread().getName(), "start")
+    print(threading.currentThread().getName(), "end")
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=worker1)
+    t2 = threading.Thread(target=worker2)
+    ti.start()
+    t2.start()
+    print("started")
