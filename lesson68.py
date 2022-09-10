2f12f10f6bd9c0db3d90719ba8f3203ec4af3a0c
Liberbell <liberbell@gmail.com>
2022-09-10 10:26:24 +0900

    lesson6810

--- lesson68.py
+++ lesson68.py
@@ -9,23 +9,17 @@ logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
 
 def worker1():
     logging.debug("start")
-    # print(threading.currentThread().getName(), "start")
     time.sleep(0.5)
     logging.debug("end")
-    # print(threading.currentThread().getName(), "end")
 
-def worker2(x, y=1):
+def worker2():
     logging.debug("start")
-    logging.debug(x)
-    logging.debug(y)
-    # print(threading.currentThread().getName(), "start")
     time.sleep(0.5)
     logging.debug("end")
-    # print(threading.currentThread().getName(), "end")
 
 if __name__ == "__main__":
-    t1 = threading.Thread(name="rename worker1", target=worker1)
-    t2 = threading.Thread(target=worker2, args=(100, ), kwargs={"y": 200})
+    t1 = threading.Thread(target=worker1)
+    t2 = threading.Thread(target=worker2)
     t1.start()
     t2.start()
     print("started")
