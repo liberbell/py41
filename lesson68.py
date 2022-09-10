9b72e0587cd6f089bf441d001a55d3ef7d0fa6f7
Liberbell <liberbell@gmail.com>
2022-09-10 10:14:27 +0900

    lesson685

--- lesson68.py
+++ lesson68.py
@@ -1,15 +1,24 @@
+from distutils.log import debug
+from doctest import master
+import logging
 import threading
 import time
 
+logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
+
 def worker1():
-    print(threading.currentThread().getName(), "start")
+    logging.debug("start")
+    # print(threading.currentThread().getName(), "start")
     time.sleep(0.5)
-    print(threading.currentThread().getName(), "end")
+    logging.debug("end")
+    # print(threading.currentThread().getName(), "end")
 
 def worker2():
-    print(threading.currentThread().getName(), "start")
+    logging.debug("start")
+    # print(threading.currentThread().getName(), "start")
     time.sleep(0.5)
-    print(threading.currentThread().getName(), "end")
+    logging.debug("end")
+    # print(threading.currentThread().getName(), "end")
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=worker1)
