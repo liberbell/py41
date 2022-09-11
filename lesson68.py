843d98f68f475d3b81148d2baa028b2a1f64ae3d
Liberbell <liberbell@gmail.com>
2022-09-11 11:40:14 +0900

    lesson682

--- lesson68.py
+++ lesson68.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         t = threading.Thread(target=worker1)
         t.setDaemon(True)
         t.start()
-
+    print(threading.enumerate())
     for thread in threading.enumerate():
         if thread is threading.current_thread():
             print(thread)
