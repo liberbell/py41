a264ea4c328e42ee893b7f25288a4484b2dcc4c5
Liberbell <liberbell@gmail.com>
2022-09-13 08:46:28 +0900

    lesson6817

--- lesson68.py
+++ lesson68.py
@@ -62,7 +62,7 @@ def worker3(barrier):
 
 if __name__ == "__main__":
     barrier = threading.Barrier(2)
-    t1 = threading.Thread(target=worker1, args=barrier,))
+    t1 = threading.Thread(target=worker1, args=(barrier,))
     t2 = threading.Thread(target=worker2, args=(barrier,))
     # t3 = threading.Thread(target=worker3, args=(barrier,))
     t1.start()
