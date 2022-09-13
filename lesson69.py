b2787648fd037e345b99a4763627c1a4f2b3c096
Liberbell <liberbell@gmail.com>
2022-09-14 08:38:44 +0900

    lesson696

--- lesson69.py
+++ lesson69.py
@@ -13,6 +13,7 @@ logging.basicConfig(level=logging.DEBUG, format="%(processName)s: %(message)s")
 def worker1(i):
     logging.debug("start")
     logging.debug(i)
+    time.sleep(3)
     logging.debug("end")
 
 def worker2(i):
@@ -23,6 +24,8 @@ def worker2(i):
 if __name__ == "__main__":
     i = 10
     t1 = multiprocessing.Process(target=worker1, args=(i, ))
+    t1.daemon()
     t2 = multiprocessing.Process(name="renaem worker2", target=worker2, args=(i, ))
     t1.start()
     t2.start()
+    t1.join()
