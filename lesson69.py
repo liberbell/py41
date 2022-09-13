de28fd4a723e1638fdf8486d8febdd8bee052db7
Liberbell <liberbell@gmail.com>
2022-09-14 08:25:33 +0900

    lesson692

--- lesson69.py
+++ lesson69.py
@@ -2,3 +2,7 @@ from multiprocessing import (
     Process, Lock, RLock, Semaphore, Queue, Event, Condition, Barrier,
     Value, Array, Pipe, Manager
 )
+
+import logging
+import threading
+import time
