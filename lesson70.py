03f7a2f7489382eca1e73cc3667d532ae86fef46
Liberbell <liberbell@gmail.com>
2022-09-15 08:31:04 +0900

    lesson702

--- lesson70.py
+++ lesson70.py
@@ -4,3 +4,10 @@ import threading
 import multiprocessing
 
 logging.basicConfig(level=logging.DEBUG, format="%{threadName}s: %{messages}s")
+
+def worker1(d, lock):
+    with lock:
+        i = d["x"]
+        time.sleep(2)
+        d["x"] = i + 1
+    logging.debug(d)
