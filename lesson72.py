e3fc5a23de31630b16df409929faee1c221456e5
Liberbell <liberbell@gmail.com>
2022-09-16 08:46:05 +0900

    lesson721

---
+++ lesson72.py
@@ -0,0 +1,11 @@
+import logging
+import time
+import threading
+import multiprocessing
+
+logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+def worker1():
+    pass
+
+if __name__ == "__main__":
