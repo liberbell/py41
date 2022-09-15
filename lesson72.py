a1db78b02ad0281490e77f161712604b0b910beb
Liberbell <liberbell@gmail.com>
2022-09-16 08:49:33 +0900

    lesson723

--- lesson72.py
+++ lesson72.py
@@ -9,3 +9,22 @@ def worker1():
     pass
 
 if __name__ == "__main__":
+    with multiprocessing.Manager() as manager:
+        l = manager.list()
+        d = manager.dict()
+        n = manager.Namespace()
+
+        l.append(1)
+        l.append(2)
+        l.append(3)
+
+        d["x"] = 0
+        n, y = 0
+
+        p1 = multiprocessing.Process(target=worker1, args=(l, d, n))
+        p1.start()
+        p1.join()
+
+        logging.debug(l)
+        logging.debug(d)
+        logging.debug(n)
