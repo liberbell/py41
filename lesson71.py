d6f013c4901b1cf1b3d3742c897c0d21bf73abab
Liberbell <liberbell@gmail.com>
2022-09-16 08:40:31 +0900

    lesson715

--- lesson71.py
+++ lesson71.py
@@ -8,6 +8,9 @@ logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
 def f(num, arr):
     logging.debug(num)
     num.value += 1.0
+    logging.debug(arr)
+    for i in range(len(arr)):
+        arr[i] *= 2
     # conn.send(("test"))
     # time.sleep(2)
     # conn.close()
@@ -16,6 +19,7 @@ if __name__ == "__main__":
     num = multiprocessing.Value("f", 0.0)
     arr = multiprocessing.Array("i", [1, 2, 3, 4, 5])
     p = multiprocessing.Process(target=f, (num, arr))
+    p.start()
     # parent_conn, child_conn = multiprocessing.Pipe()
     # p = multiprocessing.Process(target=f, args=(parent_conn, ))
     # p.start()
