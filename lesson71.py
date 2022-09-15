f68deb300ab767007f51a26c094be306d20e0982
Liberbell <liberbell@gmail.com>
2022-09-16 08:32:05 +0900

    lesson712

--- lesson71.py
+++ lesson71.py
@@ -1,10 +1,7 @@
-from ast import main
 import logging
 import time
 import threading
 import multiprocessing
-from tkinter.tix import MAIN
-from turtle import mainloop
 
 logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
 
