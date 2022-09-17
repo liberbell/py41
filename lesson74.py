e2ea7d4e4e76f0b62d3f360126ca62e2ffe2da00
Liberbell <liberbell@gmail.com>
2022-09-17 15:31:16 +0900

    lesson741

--- lesson74.py
+++ lesson74.py
@@ -4,3 +4,6 @@ import multiprocessing
 import concurrent.futures
 import logging
 import time
+
+logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(processName)s: %(message)s")
