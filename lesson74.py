3df78d32309881505e2eeb55ecc090c6d6f01971
Liberbell <liberbell@gmail.com>
2022-09-17 15:39:15 +0900

    lesson745

--- lesson74.py
+++ lesson74.py
@@ -8,6 +8,11 @@ import time
 logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
 logging.basicConfig(level=logging.DEBUG, format="%(processName)s: %(message)s")
 
+def worker(x, y):
+    logging.debug("start")
+    r = x * y
+    logging.debug("end")
+
 def main():
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         f1 = executor.submit(worker, 2, 5)
