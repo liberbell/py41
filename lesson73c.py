0f1ff16ded248ef6f27a8c2683c2657007636d2d
Liberbell <liberbell@gmail.com>
2022-09-17 15:18:53 +0900

    lesson73c2

--- lesson73c.py
+++ lesson73c.py
@@ -1,4 +1,5 @@
 from multiprocessing.managers import BaseManager
+import queue
 
 class QueueManager(BaseManager):
     pass
@@ -6,3 +7,6 @@ class QueueManager(BaseManager):
 QueueManager.register("get_queue")
 
 manager = QueueManager(address=("127.0.0.1"), authkey="thisisakey")
+manager.connect()
+queue = manager.get_queue()
+queue.put("hello")
