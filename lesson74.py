c96a286c03fac71a61a1fb939647bf7c8ba709fa
Liberbell <liberbell@gmail.com>
2022-09-17 15:38:31 +0900

    lesson743

--- lesson74.py
+++ lesson74.py
@@ -9,7 +9,8 @@ logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
 logging.basicConfig(level=logging.DEBUG, format="%(processName)s: %(message)s")
 
 def main():
-    pass
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        f1 = executor.submit(worker, 2, 5)
 
 if __name__ == "__main__":
     main()
