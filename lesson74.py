dd081538db65cdec737e6916a070dfae4d88bb04
Liberbell <liberbell@gmail.com>
2022-09-17 15:40:36 +0900

    lesson745

--- lesson74.py
+++ lesson74.py
@@ -13,10 +13,14 @@ def worker(x, y):
     r = x * y
     logging.debug(r)
     logging.debug("end")
+    return r
 
 def main():
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         f1 = executor.submit(worker, 2, 5)
+        f2 = executor.submit(worker, 2, 5)
+        logging.debug(f1.result)
+        logging.debug(f2.result)
 
 if __name__ == "__main__":
     main()
