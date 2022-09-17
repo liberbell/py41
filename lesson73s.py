976f7c09589ec45a4ae37d3332662fc6d8f98e90
Liberbell <liberbell@gmail.com>
2022-09-17 15:02:12 +0900

    lesson73s2

--- lesson73s.py
+++ lesson73s.py
@@ -1,3 +1,12 @@
 from operator import imod
 import queue
 from multiprocessing.managers import BaseManager
+
+queue = queue.Queue()
+
+class QueueManager(BaseManager):
+    pass
+
+QueueManager.register("get_queue", callable=lambda: queue)
+
+manager = QueueManager(address=("127.0.0.1", 50000), authkey=b"thisisakey")
