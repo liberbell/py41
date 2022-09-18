8d6838f3038c3dc9e33d2553a77c9c161bf82cc2
Liberbell <liberbell@gmail.com>
2022-09-18 16:39:41 +0900

    lesson756

--- lesson75.py
+++ lesson75.py
@@ -10,7 +10,7 @@ key = "".join(
 )
 
 iv = "".join(
-    random.choice(string.ascii_letters) for _ in range(AES.block_size)
+    random.choice(string.ascii_ßletters) for _ in range(AES.block_size)
 )
 print(key, iv)
 
@@ -21,3 +21,6 @@ padding_length = AES.block_size - len(plaintext) % AES.block_size
 plaintext += chr(padding_length) * padding_length
 chipher_text = cipher.encrypt(plaintext)
 print(chipher_text)
+
+cipher2 = AES.new(key, AES.MODE_CBC, iv)
+decrypted_text = cipher2.decrypt(chipher_text)
