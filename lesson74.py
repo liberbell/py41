d0ab7bb8d471884978bde5f2f2630fd2307cf608
Liberbell <liberbell@gmail.com>
2022-09-18 10:29:09 +0900

    lesson744

--- lesson74.py
+++ lesson74.py
@@ -5,8 +5,8 @@ import concurrent.futures
 import logging
 import time
 
-logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
-# logging.basicConfig(level=logging.DEBUG, format="%(processName)s: %(message)s")
+# logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(processName)s: %(message)s")
 
 def worker(x, y):
     logging.debug("start")
@@ -17,15 +17,15 @@ def worker(x, y):
 
 def main():
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        # f1 = executor.submit(worker, 2, 5)
-        # f2 = executor.submit(worker, 2, 5)
-        # logging.debug(f1.result)
-        # logging.debug(f2.result)
+        f1 = executor.submit(worker, 2, 5)
+        f2 = executor.submit(worker, 2, 5)
+        logging.debug(f1.result)
+        logging.debug(f2.result)
 
-        args = [[2, 2], [5, 5]]
-        r = executor.map(worker, *args)
-        logging.debug(r)
-        logging.debug([i for i in r])
+        # args = [[2, 2], [5, 5]]
+        # r = executor.map(worker, *args)
+        # logging.debug(r)
+        # logging.debug([i for i in r])
 
 if __name__ == "__main__":
     main()
