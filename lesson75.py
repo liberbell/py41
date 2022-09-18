438f179437b29a0cc84e2f2611abaf6f51db3cc3
Liberbell <liberbell@gmail.com>
2022-09-18 16:32:25 +0900

    lesson753

--- lesson75.py
+++ lesson75.py
@@ -3,6 +3,9 @@ import random
 
 from Crypto.Cipher import AES
 
+print(AES.block_size)
+print(string.ascii_letters)
 key = "".join(
     random.choice(string.ascii_letters) for _ in range(AES.block_size)
 )
+print(key)
