1475b13f8945bc2e603c5a3e07c29326b6c0a8b4
Liberbell <liberbell@gmail.com>
2022-09-19 14:06:39 +0900

    lesson762

--- lesson76.py
+++ lesson76.py
@@ -1,3 +1,13 @@
 import hashlib
+from hmac import digest
 
-print(hashlib.sha256(b"password").hexdigest())
+# print(hashlib.sha256(b"password").hexdigest())
+
+user_name = "user1"
+user_pass = "password"
+db = {}
+
+password = bytes(user_pass, "utf-8")
+digest = hashlib.sha256(password).hexdigest()
+db[user_name] = digest
+print(db)
