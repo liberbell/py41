b56152604860834e3cd465207b08e357434d53ec
Liberbell <liberbell@gmail.com>
2022-09-19 14:08:22 +0900

    lesson764

--- lesson76.py
+++ lesson76.py
@@ -7,7 +7,10 @@ user_name = "user1"
 user_pass = "password"
 db = {}
 
-password = bytes(user_pass, "utf-8")
-digest = hashlib.sha256(password).hexdigest()
-db[user_name] = digest
+def get_digest(password):
+    password = bytes(user_pass, "utf-8")
+    digest = hashlib.sha256(password).hexdigest()
+    return digest
+
+db[user_name] = get_digest(user_pass)
 print(db)
