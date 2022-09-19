f08fc465857bb98d2a56862b5fa3180a101f763a
Liberbell <liberbell@gmail.com>
2022-09-19 14:20:43 +0900

    lesson769

--- lesson76.py
+++ lesson76.py
@@ -8,9 +8,13 @@ user_name = "user1"
 user_pass = "password"
 db = {}
 
+salt = base64.b64encode(os.urandom(32))
+# print(salt)
+
 def get_digest(password):
     password = bytes(password, "utf-8")
-    digest = hashlib.sha256(password).hexdigest()
+    digest = hashlib.sha256(salt + password).hexdigest()
+    print(digest)
     return digest
 
 db[user_name] = get_digest(user_pass)
